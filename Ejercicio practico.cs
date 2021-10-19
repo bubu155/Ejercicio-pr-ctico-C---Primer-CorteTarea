407b56510573880e7a7d7cf9b632652ebf0c77b9
bubu155 <55672539+bubu155@users.noreply.github.com>
2021-10-19 18:23:46 -0400

    Add files via upload

---
+++ Ejercicio practico.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_practico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;  
+            int b = 4;
+            int c = 1;
+
+            Console.WriteLine("aacb");
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+
+            Console.WriteLine("--------");
+
+            Console.WriteLine("bca");
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+            Console.WriteLine("--------");
+
+            Console.WriteLine("bcbc");
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("--------");
+
+            Console.Write("end");
+
+        }
+    }
+}
